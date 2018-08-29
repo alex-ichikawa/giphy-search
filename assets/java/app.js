2200bf8066aabd03e2a0381569e4bde9dfdc253d
Alex Ichikawa <aichikawa@isolitesystems.com>
2018-08-29 15:07:53 -0700

    got giphy to post to page, working on click to animate

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,4 +1,4 @@
-const buttonChoices = ["Formula 1", "Ferrari F1", "Red Bull F1", "Aryton Senna", "Michael Schumacher", "Kimi Raikkonen", "Fernando Alonso", "Jenson Button", "Mark Webber"]
+const buttonChoices = ["Dog", "Cat", "Mouse", "Formula 1", "Ferrari F1", "Red Bull F1", "Aryton Senna", "Michael Schumacher", "Kimi Raikkonen", "Fernando Alonso", "Jenson Button", "Mark Webber"]
 
 
 let searchTerm = '';
@@ -13,25 +13,33 @@ function pushButtons() {
 
 };
 
+function giphyClicked(giphy_id) {
+    let j = giphy_id;
+      $(`"#${j}"`).html(outputAni[j]);//`<img id="${j}" onClick="giphyClicked(this.id)" src="${outputAni[j].data.images.downsized_small.url}"></img>`)
+ };
+
 function buttonClicked(button_id) {
     searchTerm = button_id.replace(' ', '+');
     URL = "https://api.giphy.com/v1/gifs/search?api_key=LLHzPvE7ufGbvRc80wsluvMS4Ub9hVwu&q=" + searchTerm + "&limit=10&offset=0&rating=Y&lang=en";
-    let output = [];
+    let outputStill = [];
+    let outputAni = [];
     $.ajax({
         url: URL,
         method: "GET"
-    }).then (function(response) {
-        output = response;
-        console.log(response);
-        console.log("1 " + output);
-    });
-    for (let i = 0; i < output.length; i++) {
-        console.log("2 " + output);
-        $("#content").append("<div>works</div>");
-    };
-};
-
-
-
+    }).then(function(response) {
+        console.log(response.data);
+        for (let i = 0; i < response.data.length; i++) {
+            outputStill.push(`<img onClick="giphyClicked(this.id)" id="${i}" src="${response.data[i].images.downsized_still.url}"></img>`);
+            outputAni.push(`<img onClick="giphyClicked(this.id)" id="${i}" src="${response.data[i].images.downsized_small.url}"></img>`);
+        };
+        $("#content").html(outputStill.join(''));
+        
+    // }).then(function giphyClicked(giphy_id) {
+    //     let j = giphy_id;
+    //       $(`"#${j}"`).html(outputAni[j]);//`<img id="${j}" onClick="giphyClicked(this.id)" src="${outputAni[j].data.images.downsized_small.url}"></img>`)
+    //  });
+    
+});
+}
 
 pushButtons(buttonChoices);
