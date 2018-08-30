e88999c397c977d3c23774807b5ced4fe02c164a
Alex Ichikawa <aichikawa@isolitesystems.com>
2018-08-29 21:30:41 -0700

    min requirements met

--- assets/java/app.js
+++ assets/java/app.js
@@ -33,16 +33,20 @@ $("#submit").on("click", function(){
 //when button is clicked calls the giphy API and searches by the value of the button, pushes to the DOM
 function buttonClicked(button_id) {
     searchTerm = button_id.replace(' ', '+');
-    URL = "https://api.giphy.com/v1/gifs/search?api_key=LLHzPvE7ufGbvRc80wsluvMS4Ub9hVwu&q=" + searchTerm + "&limit=10&offset=0&rating=Y&lang=en";
+    URL = "https://api.giphy.com/v1/gifs/search?api_key=LLHzPvE7ufGbvRc80wsluvMS4Ub9hVwu&q=" + searchTerm + "&limit=10&offset=0&lang=en";
     $.ajax({
         url: URL,
         method: "GET"
     }).then(function(response) {
+        console.log(response);
         outputAni = [];
         outputStill = [];
         for (let i = 0; i < response.data.length; i++) {
-            outputStill.push(`<img class="still" onClick="giphyClicked(this.id)" id="${i}" src="${response.data[i].images.downsized_still.url}"></img>`);
-            outputAni.push(`<img class="gif" onClick="giphyClicked(this.id)" id="${i}" src="${response.data[i].images.downsized.url}"></img>`);
+            outputStill.push(`<figure>
+                            <img class="still" onClick="giphyClicked(this.id)" id="${i}" src="${response.data[i].images.downsized_still.url}">
+                                <figcaption>Rating: ${response.data[i].rating}</figcaption>
+                            </figure>`);
+            outputAni.push(`<img class="gif" onClick="giphyClicked(this.id)" id="${i}" src="${response.data[i].images.downsized.url}">`);
         };
         $("#gifZone").html(outputStill.join(''));    
 });
