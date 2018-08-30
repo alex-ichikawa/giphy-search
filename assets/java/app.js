5a2f3281de056fbcbdfa50393599a8db66d7a99e
Alex Ichikawa <aichikawa@isolitesystems.com>
2018-08-29 17:48:28 -0700

    moving gif work for some reason

--- assets/java/app.js
+++ assets/java/app.js
@@ -3,7 +3,7 @@ const buttonChoices = ["Dog", "Cat", "Mouse", "Formula 1", "Ferrari F1", "Red Bu
 
 let searchTerm = '';
 let URL = '';
-
+let outputAni = [];
 
 function pushButtons() {
     for (let i = 0; i < buttonChoices.length; i++) {
@@ -14,25 +14,30 @@ function pushButtons() {
 };
 
 function giphyClicked(giphy_id) {
+    console.log(giphy_id);
     let j = giphy_id;
-      $(`"#${j}"`).html(outputAni[j]);//`<img id="${j}" onClick="giphyClicked(this.id)" src="${outputAni[j].data.images.downsized_small.url}"></img>`)
+    console.log(j);
+   // document.getElementById("0").style.visibility = 'hidden';
+    $("#"+j+"").replaceWith(outputAni[j]);//`<img id="${j}" onClick="giphyClicked(this.id)" src="${outputAni[j].data.images.downsized_small.url}"></img>`)
  };
 
 function buttonClicked(button_id) {
     searchTerm = button_id.replace(' ', '+');
     URL = "https://api.giphy.com/v1/gifs/search?api_key=LLHzPvE7ufGbvRc80wsluvMS4Ub9hVwu&q=" + searchTerm + "&limit=10&offset=0&rating=Y&lang=en";
     let outputStill = [];
-    let outputAni = [];
+    //let outputAni = [];
     $.ajax({
         url: URL,
         method: "GET"
     }).then(function(response) {
         console.log(response.data);
         for (let i = 0; i < response.data.length; i++) {
-            outputStill.push(`<img onClick="giphyClicked(this.id)" id="${i}" src="${response.data[i].images.downsized_still.url}"></img>`);
-            outputAni.push(`<img onClick="giphyClicked(this.id)" id="${i}" src="${response.data[i].images.downsized_small.url}"></img>`);
+            outputStill.push(`<img onClick="giphyClicked(this.id)" id="${i}" src="${response.data[i].images.fixed_width_small_still.url}"></img>`);
+              outputAni.push(`<img onClick="giphyClicked(this.id)" id="${i}" src="${response.data[i].images.fixed_width_small.url}"></img>`);
         };
         $("#content").html(outputStill.join(''));
+        console.log(outputStill);
+        console.log(outputAni);
         
     // }).then(function giphyClicked(giphy_id) {
     //     let j = giphy_id;
